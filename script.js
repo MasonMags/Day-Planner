db6eacb0f4bb815a5ff5093765c0967ba9bc2760
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-05-05 20:12:40 -0500

    removed timeblocks from html, created them dynamically in js

--- script.js
+++ script.js
@@ -1,5 +1,75 @@
 var currentDay = document.getElementById("currentDay")
 
+var hourlySchedule = [
+    {
+        hour: "09",
+        time: "09",
+        meridiem: "AM",
+        comment: ""
+    },
+    {
+        hour: "10",
+        time: "10",
+        meridiem: "AM",
+        comment: ""
+    },
+    {
+        hour: "11",
+        time: "11",
+        meridiem: "AM",
+        comment: ""
+    },
+    {
+        hour: "12",
+        time: "12",
+        meridiem: "PM",
+        comment: ""
+    },
+    {
+        hour: "01",
+        time: "01",
+        meridiem: "PM",
+        comment: ""
+    },
+    {
+        hour: "02",
+        time: "02",
+        meridiem: "PM",
+        comment: ""
+    },
+    {
+        hour: "03",
+        time: "03",
+        meridiem: "PM",
+        comment: ""
+    },
+    {
+        hour: "04",
+        time: "04",
+        meridiem: "PM",
+        comment: ""
+    },
+    {
+        hour: "05",
+        time: "05",
+        meridiem: "PM",
+        comment: ""
+    },
+]
+
+window.onload = getCurrentDate();
+
 function getCurrentDate() {
-currentDay.textContent = moment().format("dddd, MMMM Do") 
+    currentDay.textContent = moment().format("dddd, MMMM Do");
+    displayTimeblocks(); 
+}
+
+function displayTimeblocks() {
+    for (var i = 0; i < hourlySchedule.length; i++) {
+        var hourContainer = document.getElementById("hour-container")
+        var hourRow = document.createElement("form")
+        hourRow.setAttribute("class", "row")
+        hourContainer.append(hourRow)
+
+    }
 }
