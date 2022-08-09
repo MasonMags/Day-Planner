a16856bbddc2e5765c6131298512c3b4a8e6cfde
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-08-09 17:39:55 -0500

    creating rows for workday

--- script.js
+++ script.js
@@ -51,16 +51,21 @@ if (workEvents) {
 
 $("#currentDay").text(today)
 
-window.onload = getCurrentDate();
+planWorkday.forEach(function(timeBlock, index){
+    var timeLabel = timeBlock.time;
+    var blockColor = colorRow(timeLabel);
+    var row = '<div class="time-block" id="' +
+    index +
+    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+    timeLabel +
+    '</div><textarea class="form-control ' +
+    blockColor +
+    '">' +
+    timeBlock.event +
+    '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
 
-function getCurrentDate() {
-    currentDay.textContent = moment().format("dddd, MMMM Do");
-    displayTimeblocks(); 
-}
-
-
-
-
+    $(".container").append(row);
+})
 // function init() {
 //     var storedReminders = JSON.parse(localStorage.getItem("reminders"));
 
